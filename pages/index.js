173902edf2936864b2526329b00907fb94a32d87
ppfurtado <furtado.pedropaulo@gmail.com>
2021-07-13 23:17:58 -0300

    add Aluralib

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,40 @@
-import styled from "styled-components";
 import MainGrid from "../src/components/MainGrid";
 import Box from "../src/components/Box";
+import { AlurakutMenu } from "../src/lib/AluraCommons";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
+function ProfileSidebar({ githubUser }) {
+  return (
+    <Box>
+      <img
+        style={{ borderRadius: "50%" }}
+        src={`https://github.com/${githubUser}.png`}
+        alt="avatar Profile"
+      />
+    </Box>
+  );
+}
 
 export default function Home() {
+  const githubUser = "ppfurtado";
+
   return (
-    <MainGrid>
-      <div className="profileArea" style={{ gridArea: "profileArea" }}>
-        <Box>
-          <img src="https://github.com/ppfurtado.png" alt="avatar Profile" />
-        </Box>
-      </div>
-      <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
-        <Box>Bem vindo</Box>
-      </div>
-      <div
-        className="profileRelationArea"
-        style={{ gridArea: "profileRelationArea" }}
-      >
-        <Box>Pessoas da Comunidade</Box>
-        <Box>Comunidade</Box>
-      </div>
-    </MainGrid>
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>Bem vindo</Box>
+        </div>
+        <div
+          className="profileRelationArea"
+          style={{ gridArea: "profileRelationArea" }}
+        >
+          <Box>Pessoas da Comunidade</Box>
+          <Box>Comunidade</Box>
+        </div>
+      </MainGrid>
+    </>
   );
 }
