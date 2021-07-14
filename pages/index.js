0300ef4965cec8d10f5ab9b50671d2b5edd38e75
ppfurtado <furtado.pedropaulo@gmail.com>
2021-07-13 22:48:07 -0300

    create component Main Grid and Box

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,30 @@
 import styled from "styled-components";
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `;
 
-const Box = styled.div`
-  background: #ffffff;
-  border-radius: 8px;
-`;
-
 export default function Home() {
   return (
-    <main>
-      <Box>Image</Box>
-      <Box>Bem vindo</Box>
-      <Box>Comunidade</Box>
-    </main>
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: "profileArea" }}>
+        <Box>
+          <img src="https://github.com/ppfurtado.png" alt="avatar Profile" />
+        </Box>
+      </div>
+      <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+        <Box>Bem vindo</Box>
+      </div>
+      <div
+        className="profileRelationArea"
+        style={{ gridArea: "profileRelationArea" }}
+      >
+        <Box>Pessoas da Comunidade</Box>
+        <Box>Comunidade</Box>
+      </div>
+    </MainGrid>
   );
 }
